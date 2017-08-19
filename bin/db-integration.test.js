3f6ca2ea4f50f18a30fd9f366d60291a92e8da75
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2017-08-19 15:17:31 +0200

    Add DB integration for dev env

---
+++ bin/db-integration.test.js
@@ -0,0 +1,32 @@
+var models = require('./../models');
+
+models.sequelize.sync().then(function() {
+  models.account.create({
+    email: "khang@dogbiz.com",
+    is_admin: true,
+    password: "1234567890",
+    password_confirm: "1234567890"
+  }).then(function(account) {
+    return models.admin.create({
+      fullname: "Khang Nguyen",
+      account_id: account.id
+    })
+  });
+
+  models.breed.create({
+    name: "Pug",
+    note: "A list of pug dogs"
+  }).then(function(breed) {
+    return models.dog.create({
+      name: "Ly Ly",
+      birthday: new Date(2015, 5, 14),
+      gender: false,
+      parents: "Mother Dog, Father Dog",
+      address: "Brussels Belgium",
+      note: "A pretty dog",
+      breed_id: breed.id
+    });
+  });
+}).then(function() {
+  console.log("Done syncing");
+});
