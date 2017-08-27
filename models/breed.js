6d29335f5f15819be8f07803eff2fdba4412882b
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2017-08-27 23:21:39 +0200

    modify Breed model to match the requirements on Drive

--- models/breed.js
+++ models/breed.js
@@ -2,6 +2,11 @@
 
 module.exports = function(sequelize, DataTypes) {
   var Breed = sequelize.define('breed', {
+      fci: { 
+        type: DataTypes.INTEGER, 
+        allowNull: false,
+        primaryKey: true
+      },
       name: { 
         type: DataTypes.STRING, 
         allowNull: false,
@@ -12,6 +17,59 @@ module.exports = function(sequelize, DataTypes) {
           len: {
             args: [1, 128],
             msg: "Name should be from 1 to 128 characters length"
+          }
+        }
+      },
+      section: { 
+        type: DataTypes.STRING, 
+        allowNull: true,
+        validate: {
+          len: {
+            args: [0, 128],
+            msg: "Section should be less than 128 characters length"
+          }
+        }
+      },
+      country: { 
+        type: DataTypes.STRING, 
+        allowNull: true,
+        validate: {
+          len: {
+            args: [0, 32],
+            msg: "Country should be less than 32 characters length"
+          }
+        }
+      },
+      url: { 
+        type: DataTypes.STRING, 
+        allowNull: true,
+        isUrl: true,
+        validate: {
+          len: {
+            args: [0, 128],
+            msg: "URL should be less than 128 characters length"
+          }
+        }
+      },
+      image: { 
+        type: DataTypes.STRING, 
+        allowNull: true,
+        isUrl: true,
+        validate: {
+          len: {
+            args: [0, 128],
+            msg: "Image should be less than 128 characters length"
+          }
+        }
+      },
+      pdf: { 
+        type: DataTypes.STRING, 
+        allowNull: true,
+        isUrl: true,
+        validate: {
+          len: {
+            args: [0, 128],
+            msg: "PDF should be less than 128 characters length"
           }
         }
       },
