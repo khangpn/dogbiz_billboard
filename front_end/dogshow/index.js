54a7a06a516946d20e2a4a917ad6b01c4ecd01c3
Khang Phuc Nguyen <nguyenphuckhang@gmail.com>
2018-01-14 21:53:29 +0100

    Add redux thunk to the front end

--- front_end/dogshow/index.js
+++ front_end/dogshow/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import dogShowApp from './reducers'
 import AddAchievementTool from './components/add-achievement-tool'
 
-let store = createStore(dogShowApp)
+let store = createStore(dogShowApp, applyMiddleware(thunk))
 
 const container = document.getElementById('root')
 document.addEventListener('DOMContentLoaded', () => {
